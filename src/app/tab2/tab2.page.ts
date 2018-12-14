3c2c8aaaf175045d7536760470f452ff92165df7
Kai Birkenstock <kaib1985@gmail.com>
2018-12-14 20:02:52 +0100

    Revert "Revert "fixed some things; added history and stylings""
    This reverts commit 3ebefefd0a523cfbb7d73a9fde21be6496494f22.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as moment from 'dayjs';
+import {fromJS, List} from 'immutable';
+import {AbstractPage} from '../abstracts/AbstractPage';
+import {TimeFrame} from '../models/TimeFrame';
+import {UserConfig} from '../models/UserConfig';
+import {WorkoutDay} from '../models/WorkoutDay';
+import {DatabaseService} from '../services/db/db.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page extends AbstractPage {
+  userConfig: UserConfig;
+  historyItems: List<WorkoutDay>;
+
+  constructor(private db: DatabaseService) {
+    super();
+  }
+
+  async setState() {
+    const config = await this.db.loadConfig();
+    this.userConfig = new UserConfig(fromJS(config), new TimeFrame(config.date));
+
+    this.historyItems = this.userConfig.cycle.workoutDays.filter(x => x.isDone || x.date.startOf('day').isBefore(moment().startOf('day'))).toList();
+  }
+}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { DatabaseService } from '../services/db/db.service';
-import { Router } from '@angular/router';
-import { AbstractPage } from '../abstracts/AbstractPage';
-import { Events } from '@ionic/angular';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {AlertController, Events} from '@ionic/angular';
+import * as firebase from 'firebase';
+import {AbstractPage} from '../abstracts/AbstractPage';
+import {DatabaseService} from '../services/db/db.service';
 
 @Component({
   selector: 'app-tab3',
@@ -10,15 +11,50 @@ import { Events } from '@ionic/angular';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page extends AbstractPage {
-  constructor(public db: DatabaseService, public router: Router, public events: Events) {
+  userProfile: any;
+
+  constructor(public db: DatabaseService,
+              public alertCtrl: AlertController,
+              public router: Router,
+              public events: Events) {
     super();
   }
 
+  setState() {
+    firebase.auth()
+      .onAuthStateChanged(user => {
+        if (user) {
+          this.userProfile = user;
+        } else {
+          this.userProfile = null;
+        }
+      });
+  }
+
+  async logout() {
+    await firebase.auth().signOut();
+
+    await this.router.navigateByUrl('/registration', { replaceUrl: true });
+  }
+
   async clearData() {
-    await this.db.clearUserData();
+    const confirm = await this.alertCtrl.create({
+      header: 'Warning',
+      subHeader: 'Clear all data and reset the app?',
+      buttons: [
+        {
+          text: 'OK',
+          handler: async () => {
+            await this.db.clearUserData();
 
-    await this.router.navigateByUrl('/tabs/(home:home)');
+            await this.router.navigateByUrl('/tabs/(home:home)');
 
-    this.events.publish('home:setState');
+            this.events.publish('home:setState');
+          }
+        }
+      ]
+    });
+
+    await confirm.present();
   }
 }
